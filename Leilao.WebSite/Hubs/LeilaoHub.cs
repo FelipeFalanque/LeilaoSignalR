09a969fb97b1434209131847a6ed074f86495561
Cesar Falanque <felipefalanque@gmail.com>
2019-05-19 22:21:25 -0300

    Pequeno Exemplo de como usar o SignalR, tem o que melhorar mais ja esta funcionando

---
+++ Leilao.WebSite/Hubs/LeilaoHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace Leilao.WebSite.Hubs
+{
+    [HubName("leilaohub")]
+    public class LeilaoHub : Hub
+    {
+        public void Lance(string name, int id_item)
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.LanceEfetuado(name, id_item, DateTime.Now.ToString());
+        }
+    }
+}
---
+++ Leilao.WebSite/Scripts/leilao/leilao.js
@@ -0,0 +1,37 @@
+﻿
+var chat;
+var nome;
+
+$(document).ready(function () {
+    PerguntaNome();
+    // Reference the auto-generated proxy for the hub.
+    chat = $.connection.leilaohub;
+    // Create a function that the hub can call back to display messages.
+    chat.client.LanceEfetuado = function (name, id_item, data) {
+        $("#title_item_" + id_item).text(name);
+        $("#desc_item_" + id_item).text("Ultimo lance - " + name + " " + data);
+
+        toastr.success("Novo lanche efetuado", name);
+    };
+    // Start the connection.
+    $.connection.hub.start().done(function () {
+        console.log("conectado");
+    });
+});
+
+function Teste(id_item) {
+    bootbox.confirm("Deseja da uma lance ?", function (resp) {
+        if (resp == true) {
+            chat.server.lance(nome, id_item);
+        }
+        else {
+            toastr.error("Lance não foi dado!", "Lance Anulado");
+        }
+    });
+}
+
+function PerguntaNome() {
+    bootbox.prompt("Seu Nome", function (result) {
+        nome = result;
+    });
+}
