4de3299367e747f899aa3867e8060b882fa81dff
Cesar Falanque <felipefalanque@gmail.com>
2019-05-21 21:35:28 -0300

    Melhorias

--- Leilao.WebSite/Scripts/leilao/leilao.js
+++ Leilao.WebSite/Scripts/leilao/leilao.js
@@ -1,6 +1,7 @@
 ﻿
 var chat;
 var nome;
+var ultimosLances = [];
 
 $(document).ready(function () {
     PerguntaNome();
@@ -8,8 +9,10 @@ $(document).ready(function () {
     chat = $.connection.leilaohub;
     // Create a function that the hub can call back to display messages.
     chat.client.LanceEfetuado = function (name, id_item, data) {
-        $("#title_item_" + id_item).text(name);
-        $("#desc_item_" + id_item).text("Ultimo lance - " + name + " " + data);
+        //$("#title_item_" + id_item).text(name);
+        $("#desc_item_" + id_item).text(name);
+
+        GerenciaMensagens(data + " Lance dado por "+ name);
 
         toastr.success("Novo lanche efetuado", name);
     };
@@ -31,7 +34,26 @@ function Teste(id_item) {
 }
 
 function PerguntaNome() {
-    bootbox.prompt("Seu Nome", function (result) {
+    bootbox.prompt('<p class="text-center">Qual o seu primeiro nome amigo?</p>', function(result) {
         nome = result;
     });
+}
+
+function GerenciaMensagens(msg) {
+    if (ultimosLances.length >= 3) {
+        //remove o primeiro item
+        ultimosLances.splice(0, 1);
+        //adiciona no final
+        ultimosLances.push(msg);
+    } else {
+        //adiciona no final
+        ultimosLances.push(msg);
+    }
+
+    var lis = "";
+    for (var i = 0; i < ultimosLances.length; i++) {
+        lis += "<li>" + ultimosLances[i] + "</li>"
+    }
+
+    $("#ultimos_lances").empty().append(lis);
 }
